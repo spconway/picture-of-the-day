318c9753f41cf9feadbd63b58638e764e016c6c9
Stephen Conway <stephen.conway87@yahoo.com>
2022-04-16 20:03:47 -0400

    Adding public API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  nasa_apod_api_key: 'ZBWGkl4MWSuLFySaURxV3bAah35tEOrf579lzXoB', //https://api.nasa.gov/planetary/apod?api_key=ZBWGkl4MWSuLFySaURxV3bAah35tEOrf579lzXoB
+  nasa_apod_api: 'https://api.nasa.gov/planetary/apod',
 };
